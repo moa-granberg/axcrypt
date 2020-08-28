d05d8f3166fc4d18ce86f7b135dbdf072b1fc340
Moa Granberg <moa.granberg@hotmail.com>
2020-08-28 10:26:35 +0200

    Added logos to assets

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <mq-layout mq="mobile" class="nav-mobile">
+      <router-link to="/"
+        ><img
+          src="../assets/logos/axcrypt_text.png"
+          alt="Logo"
+      /></router-link>
+    </mq-layout>
+
+    <!-- <mq-layout mq="desktop">
+      <h1>desktop</h1>
+    </mq-layout> -->
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
