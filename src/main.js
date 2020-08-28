177532e869ff58b090460ab120edd14903bb2322
Moa Granberg <moa.granberg@hotmail.com>
2020-08-28 10:24:23 +0200

    Install & setup vue-mq

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router/index'
+import router from './router/index';
+import mq from 'vue-mq';
 
 Vue.config.productionTip = false;
+
+Vue.use(mq, {
+  breakpoints: {
+    mobile: 950,
+    desktop: Infinity,
+  },
+  defaultBreakpoint: 'mobile',
+});
 
 new Vue({
   router,
