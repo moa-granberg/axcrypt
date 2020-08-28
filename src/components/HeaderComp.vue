b54a1dc70cea9f7852006b7ee759d74208b4b819
Daniel Hölbling <daniel.holbling@gmail.com>
2020-08-28 14:53:15 +0200

    Code formatting, styling modifications to nav-mobile

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -10,7 +10,7 @@
       <ul class="nav-mobile-links">
         <li v-for="link in navLinks" :key="link.path">
           <router-link :to="link.path">
-            {{link.text}}
+            {{ link.text }}
           </router-link>
         </li>
       </ul>
@@ -28,38 +28,41 @@ export default {
   components: {
     HamburgerMenu,
   },
-  mounted(){
+  mounted() {
     console.log(this.$router.options.routes);
   },
   computed: {
-    navLinks(){
+    navLinks() {
       const routes = this.$router.options.routes;
       const returnValue = routes.map(obj => {
         // return obj.name.split('View')[0].replace(/([A-Z])/g, ' $1').trim();
-        return { text: obj.name.split('View')[0].replace(/([A-Z])/g, ' $1').trim(),
-                path: obj.path
-        }
-      })
-      return returnValue.filter(item => 
-        !item.text.includes('Page')
+        return {
+          text: obj.name
+            .split('View')[0]
+            .replace(/([A-Z])/g, ' $1')
+            .trim(),
+          path: obj.path,
+        };
+      });
+      return returnValue.filter(
+        item => !item.text.includes('Page') && item.text !== 'Home'
       );
-    }
-  }
-
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../scss/variables.scss";
+@import '../scss/variables.scss';
 .logo {
   width: 145px;
-  margin: 30px 0;
 }
 
 .nav-mobile {
   display: flex;
   justify-content: space-between;
-  padding: 0 20px;
+  align-items: center;
+  padding: 20px 20px 25px 20px;
   max-width: 767px;
   margin: auto;
   border-bottom: 1px $light-gray solid;
@@ -68,10 +71,14 @@ export default {
 .nav-mobile-links {
   list-style: none;
   margin: 0;
+  margin-bottom: 20px;
   padding: 0;
   li {
     padding: 15px 25px;
     border-bottom: 1px $light-gray solid;
+    &:last-of-type > a {
+      font-weight: 400;
+    }
     a {
       font-size: 13px;
       font-weight: 300;
