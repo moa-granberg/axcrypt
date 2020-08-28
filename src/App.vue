bbf7fec6bcdd0ef95e2190020887d0b86ab53818
Daniel Hölbling <daniel.holbling@gmail.com>
2020-08-28 13:47:53 +0200

    Added sass variables, general styling

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,13 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap');
+@import './scss/variables.scss';
+
 body {
+  font-family: 'Open Sans', sans-serif;
   margin: 0;
+  color: $gray;
 }
 </style>
