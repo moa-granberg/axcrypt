0472b6c3aec474d5515e413ac2180e56bbbe7c83
Daniel Hölbling <daniel.holbling@gmail.com>
2020-09-02 15:44:43 +0200

    Contact us, mobile styling and phraseKeys

--- src/components/footer/FooterContact.vue
+++ src/components/footer/FooterContact.vue
@@ -1,9 +1,44 @@
 <template>
-  <div></div>
+  <section :class="'footer-contact ' + $mq">
+    <h3 :class="'footer-contact-heading ' + $mq">{{ $t('ContactUsHeading') }}</h3>
+    <p 
+      :class="'footer-contact-paragraph ' + $mq" 
+      v-html="$t('FooterContactUsParagraph1')"
+    />
+
+    <p 
+      :class="'footer-contact-paragraph ' + $mq"
+      v-html="$t('FooterContactUsParagraph2')" 
+    />
+  </section>
 </template>
 
-<script>
-export default {};
-</script>
 
-<style></style>
+
+<style lang="scss">
+@import '@/scss/variables.scss';
+
+.footer-contact {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer-contact-heading {
+  font-size: 0.875rem;
+  margin: 24px 0 18px 0;
+}
+
+.footer-contact-paragraph {
+  text-align: center;
+  font-size: 0.75rem;
+  line-height: 1.2rem;
+  max-width: 327px;
+  a {
+    color: $white;
+    font-weight: 600;
+    text-decoration: none;
+  }
+}
+</style>
