1d399c3725a2050e9a055909d1fd87cbbef2a7bb
Moa Granberg <moa.granberg@hotmail.com>
2020-09-03 10:10:03 +0200

    Removal of footer-try-btn, not a footer comp

--- src/components/footer/FooterComp.vue
+++ src/components/footer/FooterComp.vue
@@ -4,7 +4,6 @@
     <footer-sitemap />
     <footer-contact />
     <footer-social />
-    <footer-try-btn />
   </footer>
 </template>
 
@@ -12,14 +11,12 @@
 import FooterSitemap from './FooterSitemap';
 import FooterContact from './FooterContact';
 import FooterSocial from './FooterSocial';
-import FooterTryBtn from './FooterTryBtn';
 
 export default {
   components: {
     FooterSitemap,
     FooterContact,
     FooterSocial,
-    FooterTryBtn,
   },
 };
 </script>
