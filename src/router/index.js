cf1a16af8c7ae7efd4e7754526c09775ad4ac7f5
Moa Granberg <moa.granberg@hotmail.com>
2020-09-04 13:47:26 +0200

    Meta titles

--- src/router/index.js
+++ src/router/index.js
@@ -10,81 +10,129 @@ const routes = [
     path: '/',
     name: 'HomeView',
     component: Views.HomeView,
+    meta: {
+      title: ' ',
+    },
   },
   {
     path: '/download',
     name: 'DownloadView',
     component: Views.DownloadView,
+    meta: {
+      title: 'Download | ',
+    },
   },
   {
     path: '/business',
     name: 'BusinessView',
     component: Views.BusinessView,
+    meta: {
+      title: 'Business | ',
+    },
   },
   {
     path: '/premium',
     name: 'PremiumView',
     component: Views.PremiumView,
+    meta: {
+      title: 'Premium | ',
+    },
   },
   {
     path: '/pricing',
     name: 'PricingView',
     component: Views.PricingView,
+    meta: {
+      title: 'Pricing | ',
+    },
   },
   {
     path: '/information',
     name: 'InformationView',
     component: Views.InformationView,
+    meta: {
+      title: 'Information | ',
+    },
     children: [
       {
         path: 'get-started/:platform',
         name: 'GetStartedPage',
         component: Pages.GetStartedPage,
+        meta: {
+          title: 'Get Started | ',
+        },
       },
       {
         path: 'features',
         name: 'FeaturesPage',
         component: Pages.FeaturesPage,
+        meta: {
+          title: 'Features | ',
+        },
       },
       {
         path: 'requirements',
         name: 'RequirementsPage',
         component: Pages.RequirementsPage,
+        meta: {
+          title: 'Requirements | ',
+        },
       },
       {
         path: 'reseller',
         name: 'ResellerPage',
         component: Pages.ResellerPage,
+        meta: {
+          title: 'Reseller | ',
+        },
       },
       {
         path: 'usage-tips',
         name: 'UsageTipsPage',
         component: Pages.UsageTipsPage,
+        meta: {
+          title: 'Usage Tips | ',
+        },
       },
       {
         path: 'security',
         name: 'SecurityPage',
         component: Pages.SecurityPage,
+        meta: {
+          title: 'Security | ',
+        },
       },
       {
         path: 'technical-details',
         name: 'TechnicalDetailsPage',
         component: Pages.TechnicalDetailsPage,
+        meta: {
+          title: 'Technical Details | ',
+        },
       },
       {
         path: 'release-notes',
         name: 'ReleaseNotesPage',
         component: Pages.ReleaseNotesPage,
+        meta: {
+          title: 'Release Notes | ',
+        },
       },
       {
         path: 'privacy-policy',
         name: 'PrivacyPolicyPage',
         component: Pages.PrivacyPolicyPage,
+        meta: {
+          title: 'Privacy Policy | ',
+        },
       },
       {
         path: 'terms-of-use',
         name: 'TermsOfUsePage',
         component: Pages.TermsOfUsePage,
+        meta: {
+          title: 'Terms of Use | ',
+        },
       },
     ],
   },
@@ -92,31 +140,49 @@ const routes = [
     path: '/support',
     name: 'SupportView',
     component: Views.SupportView,
+    meta: {
+      title: 'Support | ',
+    },
     children: [
       {
         path: 'faq',
         name: 'FaqPage',
         component: Pages.FaqPage,
+        meta: {
+          title: 'FAQ | ',
+        },
       },
       {
         path: 'axcrypt-business',
         name: 'AxcryptBusinessPage',
         component: Pages.AxcryptBusinessPage,
+        meta: {
+          title: 'AxCrypt Business | ',
+        },
       },
       {
         path: 'regulatory-compliance',
         name: 'RegulatoryCompliancePage',
         component: Pages.RegulatoryCompliancePage,
+        meta: {
+          title: 'Regulatory Compliance | ',
+        },
       },
       {
         path: 'password-generator',
         name: 'PasswordGeneratorPage',
         component: Pages.PasswordGeneratorPage,
+        meta: {
+          title: 'Password Generator | ',
+        },
       },
       {
         path: 'service-status',
         name: 'ServiceStatusPage',
         component: Pages.ServiceStatusPage,
+        meta: {
+          title: 'Service Status | ',
+        },
       },
     ],
   },
@@ -124,21 +190,33 @@ const routes = [
     path: '/about',
     name: 'AboutView',
     component: Views.AboutView,
+    meta: {
+      title: 'About | ',
+    },
     children: [
       {
         path: 'jobs',
         name: 'JobsPage',
         component: Pages.JobsPage,
+        meta: {
+          title: 'Jobs: Apply Now | ',
+        },
       },
       {
         path: 'jobs/:id',
         name: 'JobPositionPage',
         component: Pages.JobPositionPage,
+        meta: {
+          title: ' ',
+        },
       },
       {
         path: 'press',
         name: 'PressPage',
         component: Pages.PressPage,
+        meta: {
+          title: 'Press and Media | ',
+        },
       },
     ],
   },
@@ -146,11 +224,17 @@ const routes = [
     path: '/sign-in',
     name: 'SignInView',
     component: Views.SignInView,
+    meta: {
+      title: 'Sign In | ',
+    },
   },
   {
     path: '/application/:id',
     name: 'JobApplicationPage',
     component: Pages.JobApplicationPage,
+    meta: {
+      title: 'Application Form | ',
+    },
   },
 ];
 
