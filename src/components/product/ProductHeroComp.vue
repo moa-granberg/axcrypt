6c063988098c57bb39d170a9b8b9cf001266e86e
Daniel Hölbling <daniel.holbling@gmail.com>
2020-09-04 15:38:57 +0200

    product-hero completed for mobile

--- src/components/product/ProductHeroComp.vue
+++ src/components/product/ProductHeroComp.vue
@@ -1,13 +1,66 @@
 <template>
-  <section>This is ProductHeroComp</section>
+  <section :class="'product-hero-wrapper ' + $mq">
+    <div :class="'product-hero-headings-wrapper ' + $mq">
+      <h2 :class="'product-hero-preheading ' + $mq">AxCrypt</h2>
+      <h1 :class="'product-hero-heading ' + $mq"> {{$t('PremiumLinkLabel')}} </h1>
+      <h3 :class="'product-hero-subheading ' + $mq"> {{$t('ProductHeroSubheading')}} </h3>
+    </div>
+    <product-try-it-for-free-cta-comp />
+  </section>
 </template>
 
 <script>
-export default {
+import ProductTryItForFreeCtaComp from './ProductTryItForFreeCtaComp';
 
+export default {
+  components: {
+    ProductTryItForFreeCtaComp
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
 
+
+.product-hero-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgba(0,0,0,0.5);
+  background-image: url('~@/assets/view/premium/premium-mobile-hero.png');
+  background-blend-mode: overlay;
+  background-size: cover;
+  height: 470px;
+}
+
+.product-hero-headings-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.product-hero-preheading {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: 300;
+  color: $white;
+}
+.product-hero-heading {
+  margin: 6px 0 14px 0;
+  font-size: 2.5rem;
+  color: $light-green;
+  text-transform: uppercase;
+  text-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+}
+.product-hero-subheading {
+  margin: 0;
+  font-size: 1.125rem;
+  font-weight: 300;
+  font-style: italic;
+  color: $white;
+}
 </style>
---
+++ src/components/product/ProductTryItForFreeCtaComp.vue
@@ -0,0 +1,57 @@
+<template>
+  <article :class="'product-try-it-for-free-cta-wrapper ' + $mq">
+    <h1 :class="'product-try-it-for-free-cta-heading ' + $mq">{{ $t('ProductTryItForFreeHeading') }}</h1>
+    <h2 :class="'product-try-it-for-free-cta-subheading ' + $mq">{{ $t('ProductTryItForFreeSubheading') }}</h2>
+    <button :class="'product-try-it-for-free-cta-button ' + $mq">{{ $t('ProductTryItForFreeButtonLabel') }}</button>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.product-try-it-for-free-cta-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 29px 0;
+  color: $white;
+  &.mobile {
+    background: linear-gradient(
+      to bottom,
+      rgba(73, 119, 44, 0.7) 0%,
+      rgba(116, 144, 98, 0.9) 45%,
+      rgba(116, 144, 98, 0.9) 55%,
+      rgba(72, 119, 44, 0.7) 100%
+    );
+  }
+}
+
+.product-try-it-for-free-cta-heading {
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+
+.product-try-it-for-free-cta-subheading {
+  margin: 10px;
+  font-size: 1.125rem;
+  font-weight: 300;
+}
+
+.product-try-it-for-free-cta-button {
+  font-size: 1.125rem;
+  font-family: $standard-font;
+  color: $white;
+  background-color: $light-green;
+  border: 0;
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+  padding: 18px 50px;
+}
+</style>
