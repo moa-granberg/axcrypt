fa0adfa401acfce3f3a5eacb31fccecd9ba1a90e
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-07 13:35:33 +0200

    Styling premium business cta

--- src/components/product/ProductPremiumBusinessCtaComp.vue
+++ src/components/product/ProductPremiumBusinessCtaComp.vue
@@ -1,7 +1,7 @@
 <template>
-  <section>
-    <h2> {{$t('PremiumBusinessCtaPreHeading')}} </h2>
-    <h1> {{$t('PremiumBusinessCtaHeading')}} </h1>
+  <section :class="'premium-business-cta-wrapper ' + $mq">
+    <h2 :class="'premium-business-cta-preheading ' + $mq"> {{$t('PremiumBusinessCtaPreHeading')}} </h2>
+    <h1 :class="'premium-business-cta-heading ' + $mq"  v-html="$t('PremiumBusinessCtaHeading')"></h1>
     <primary-button-comp phraseKey="ReadMoreButtonLabel" size="small" />
   </section>
 </template>
@@ -16,6 +16,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
 
+.premium-business-cta-wrapper {
+  background-color: rgba(0,0,0,0.7);
+  color: $white;
+  text-align: center;
+  padding: 28px 0 0 0;
+}
+
+.premium-business-cta-preheading {
+  font-size: 0.875rem;
+  font-weight: 400;
+  margin: 0;
+}
+
+.premium-business-cta-heading {
+  font-size: 1.125rem;
+  font-weight: 400;
+  margin: 7px 0 0 0;
+}
 </style>
