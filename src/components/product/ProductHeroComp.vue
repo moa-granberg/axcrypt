e926a93d36b9655e6dfbbd5ad7ea401b3e8df59d
Moa Granberg <moa.granberg@hotmail.com>
2020-09-07 15:12:05 +0200

    Premium hero-comp desktop

--- src/components/product/ProductHeroComp.vue
+++ src/components/product/ProductHeroComp.vue
@@ -9,7 +9,9 @@
         {{ $t('ProductHeroSubheading') }}
       </h3>
     </div>
-    <product-try-it-for-free-cta-comp />
+
+    <product-try-it-for-free-cta-comp v-if="$mq === 'mobile'" />
+    <div v-else></div>
   </section>
 </template>
 
@@ -27,15 +29,25 @@ export default {
 @import '@/scss/variables.scss';
 
 .product-hero-wrapper {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  background-image: url('~@/assets/view/premium/premium-mobile-hero.png');
   background-color: rgba(0, 0, 0, 0.5);
   background-blend-mode: overlay;
   background-size: cover;
   height: 470px;
+
+  &.mobile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    background-image: url('~@/assets/view/premium/premium-mobile-hero.png');
+  }
+
+  &.desktop {
+    display: grid;
+    grid: 1fr / 1fr 1fr;
+    justify-items: flex-end;
+    background-image: url('~@/assets/view/premium/premium-desktop-hero.png');
+  }
 }
 
 .product-hero-headings-wrapper {
@@ -48,22 +60,47 @@ export default {
 
 .product-hero-preheading {
   margin: 0;
-  font-size: 1.5rem;
   font-weight: 300;
   color: $white;
+
+  &.mobile {
+    font-size: 1.5rem;
+  }
+
+  &.desktop {
+    font-size: 3rem;
+  }
 }
+
 .product-hero-heading {
   margin: 6px 0 14px 0;
-  font-size: 2.5rem;
   color: $light-green;
   text-transform: uppercase;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  &.mobile {
+    font-size: 2.5rem;
+  }
+
+  &.desktop {
+    font-size: 6.25rem;
+    letter-spacing: -4px;
+    line-height: 1;
+  }
 }
+
 .product-hero-subheading {
   margin: 0;
-  font-size: 1.125rem;
   font-weight: 300;
   font-style: italic;
   color: $white;
+
+  &.mobile {
+    font-size: 1.125rem;
+  }
+
+  &.desktop {
+    font-size: 1.5rem;
+  }
 }
 </style>
