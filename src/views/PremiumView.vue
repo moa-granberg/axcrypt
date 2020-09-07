5d86120d89821b322a90c9f75b8b6f49e72bc175
Moa Granberg <moa.granberg@hotmail.com>
2020-09-07 15:13:34 +0200

    Premium desktop-grid layout body & aside

--- src/views/PremiumView.vue
+++ src/views/PremiumView.vue
@@ -1,11 +1,41 @@
 <template>
   <main>
     <product-hero-comp />
-    <product-body-comp headerPhraseKey="PremiumFirstBodyHeading" bodyPhraseKey="PremiumFirstBodyText" />
-    <product-try-it-for-free-comp />
-    <product-body-comp headerPhraseKey="PremiumSecondBodyHeading" bodyPhraseKey="PremiumSecondBodyText" />
+
+    <mq-layout mq="desktop" class="product-grid-wrapper">
+      <product-body-comp
+        class="grid-body1"
+        headerPhraseKey="PremiumFirstBodyHeading"
+        bodyPhraseKey="PremiumFirstBodyText"
+      />
+      <div class="product-try-it-for-free-desktop-wrapper">
+        <product-try-it-for-free-cta-comp />
+        <product-try-it-for-free-comp />
+      </div>
+      <product-body-comp
+        class="grid-body2"
+        headerPhraseKey="PremiumSecondBodyHeading"
+        bodyPhraseKey="PremiumSecondBodyText"
+      />
+    </mq-layout>
+
+    <mq-layout mq="mobile">
+      <product-body-comp
+        headerPhraseKey="PremiumFirstBodyHeading"
+        bodyPhraseKey="PremiumFirstBodyText"
+      />
+      <product-try-it-for-free-comp />
+      <product-body-comp
+        headerPhraseKey="PremiumSecondBodyHeading"
+        bodyPhraseKey="PremiumSecondBodyText"
+      />
+    </mq-layout>
+
     <product-premium-business-cta-comp />
-    <product-body-comp headerPhraseKey="PremiumThirdBodyHeading" bodyPhraseKey="PremiumThirdBodyText" />
+    <product-body-comp
+      headerPhraseKey="PremiumThirdBodyHeading"
+      bodyPhraseKey="PremiumThirdBodyText"
+    />
   </main>
 </template>
 
@@ -13,6 +43,7 @@
 import ProductHeroComp from '@/components/product/ProductHeroComp';
 import ProductBodyComp from '@/components/product/ProductBodyComp';
 import ProductTryItForFreeComp from '@/components/product/ProductTryItForFreeComp';
+import ProductTryItForFreeCtaComp from '@/components/product/ProductTryItForFreeCtaComp';
 import ProductPremiumBusinessCtaComp from '@/components/product/ProductPremiumBusinessCtaComp';
 
 export default {
@@ -20,7 +51,47 @@ export default {
     ProductHeroComp,
     ProductBodyComp,
     ProductTryItForFreeComp,
+    ProductTryItForFreeCtaComp,
     ProductPremiumBusinessCtaComp,
-  }
-}
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.product-grid-wrapper {
+  display: grid;
+  grid-template-columns: repeat(1fr, 4);
+  grid-template-areas:
+    ' . body1 aside .'
+    ' . body2 aside .';
+  justify-items: center;
+  align-items: center;
+}
+
+.grid-body1 {
+  grid-area: body1;
+  max-width: 600px;
+}
+
+.grid-body2 {
+  grid-area: body2;
+  max-width: 600px;
+  align-self: flex-start;
+}
+
+.product-try-it-for-free-desktop-wrapper {
+  grid-area: aside;
+  position: relative;
+  top: -200px;
+  margin: 0 0 -175px 0;
+  width: 450px;
+  background: linear-gradient(
+    to bottom,
+    rgba(33, 80, 6, 0.9) 0%,
+    rgba(73, 119, 44, 0.8) 70%,
+    rgba(73, 119, 44, 0.7) 100%
+  );
+}
+</style>
