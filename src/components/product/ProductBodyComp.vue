6a73c8386a7907673570f4ef7719dd779918439a
Moa Granberg <moa.granberg@hotmail.com>
2020-09-07 09:33:15 +0200

    Premium Body-component mobile template & styling

--- src/components/product/ProductBodyComp.vue
+++ src/components/product/ProductBodyComp.vue
@@ -1,13 +1,38 @@
 <template>
-  <section>This is ProductBodyComp</section>
+  <section :class="'product-body-wrapper ' + $mq">
+    <h1 :class="'product-body-header ' + $mq">
+      AxCrypt {{ $t('PremiumLinkLabel') }}
+    </h1>
+    <div
+      :class="'product-body-text ' + $mq"
+      v-html="$t('PremiumBodyText')"
+    ></div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import '@/scss/variables.scss';
 
+.product-body-wrapper {
+  margin: 32px;
+}
+
+.product-body-header {
+  font-size: 1.125rem;
+  color: $green;
+  font-weight: 400;
+}
+
+.product-body-text {
+  font-size: 0.875rem;
+}
+
+.product-body-text > p > a {
+  text-decoration: none;
+  color: $dark-green;
+}
 </style>
