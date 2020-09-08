448c2152e3a660b8f327ca474659dccdfb21923a
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-08 11:21:00 +0200

    Business first body mobile

--- src/views/BusinessView.vue
+++ src/views/BusinessView.vue
@@ -1,15 +1,28 @@
 <template>
   <main>
-<product-hero-comp product="business" />
+    <product-hero-comp product="business" />
+
+    <mq-layout mq="mobile">
+      <product-body-comp
+        headerPhraseKey="BusinessFirstBodyHeading"
+        bodyPhraseKey="BusinessFirstBodyText"
+      />
+      <!-- <product-try-it-for-free-comp /> -->
+      <!-- Why AxCrypt Business Comp -->
+    </mq-layout>
   </main>
 </template>
 
 <script>
 import ProductHeroComp from '@/components/product/ProductHeroComp';
+import ProductBodyComp from '@/components/product/ProductBodyComp';
+// import ProductTryItForFreeComp from '@/components/product/ProductTryItForFreeComp';
 
 export default {
   components: {
     ProductHeroComp,
+    ProductBodyComp,
+    // ProductTryItForFreeComp,
   }
 }
 </script>
