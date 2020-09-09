f7b9b3dccf7f5391bdff9f3ea89c3eb6ef06c80b
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-09 13:30:53 +0200

    Added connection to google analytics with vue-router integration

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueGtag from 'vue-gtag';
 import router from './router/index';
 import mq from 'vue-mq';
 import i18n from './i18n';
 
 Vue.config.productionTip = false;
+
+Vue.use(
+  VueGtag,
+  {
+    config: {
+      id: 'UA-177556391-1',
+    },
+    pageTrackerTemplate(to) {
+      return {
+        page_title: to.meta.title + 'Axcrypt - File Security Made Easy',
+        page_path: to.path,
+      };
+    },
+  },
+  router
+);
 
 Vue.use(mq, {
   breakpoints: {
@@ -19,7 +36,9 @@ router.beforeEach((to, from, next) => {
     .slice()
     .reverse()
     .find(r => r.meta && r.meta.title);
-  if (nearestWithTitle) document.title = nearestWithTitle.meta.title + 'Axcrypt - File Security Made Easy'
+  if (nearestWithTitle)
+    document.title =
+      nearestWithTitle.meta.title + 'Axcrypt - File Security Made Easy';
   next();
 });
 
