437063708843c59515bab808c94e7e6ec2a0eb8a
Moa Granberg <moa.granberg@hotmail.com>
2020-09-10 15:16:49 +0200

    Download TryCta-comp style desktop

--- src/components/download/DownloadTryCtaComp.vue
+++ src/components/download/DownloadTryCtaComp.vue
@@ -26,14 +26,30 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 24px 0;
+  &.mobile {
+    padding: 24px 0;
+  }
+
+  &.desktop {
+    padding: 72px 0;
+  }
 }
 
 .download-try-cta-heading {
-  font-size: 0.875rem;
   font-weight: 400;
-  max-width: 170px;
-  margin: 0 0 24px 0;
+  text-align: center;
+
+  &.mobile {
+    font-size: 0.875rem;
+    max-width: 170px;
+    margin: 0 0 24px 0;
+  }
+
+  &.desktop {
+    font-size: 1.5rem;
+    max-width: 280px;
+    margin: 0 0 44px 0;
+  }
 
   span {
     font-weight: 600;
