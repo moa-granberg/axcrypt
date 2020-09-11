fd8f93b10e5d6c4b517493afca4ac735981f35f4
Moa Granberg <moa.granberg@hotmail.com>
2020-09-11 14:09:04 +0200

    Refact common button-style in primary-button served from standardButton.scss

--- src/components/PrimaryButtonComp.vue
+++ src/components/PrimaryButtonComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :class="`primary-btn ${size} ${$mq} ${color}`" :to="path">
+  <router-link :class="`standard-button ${size} ${$mq} ${color}`" :to="path">
     {{ $t(phraseKey) }}
   </router-link>
 </template>
@@ -17,49 +17,5 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
-
-.primary-btn {
-  background-color: $light-green;
-  color: $white;
-  font-weight: 400;
-  font-family: $standard-font;
-  border: none;
-  border-radius: 5px;
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
-  text-decoration: none;
-  text-align: center;
-
-  &.small {
-    padding: 10px 23px;
-
-    &.mobile {
-      font-size: 0.875rem;
-    }
-
-    &.desktop {
-      font-size: 1.125rem;
-      padding: 20px 50px;
-    }
-  }
-
-  &.large {
-    padding: 20px 50px;
-
-    &.mobile {
-      font-size: 1.125rem;
-    }
-
-    &.desktop {
-      font-size: 1.5rem;
-    }
-  }
-
-  &.dark {
-    background-color: $dark-green;
-  }
-
-  &.black {
-    background-color: rgba(0, 0, 0, 0.9);
-  }
-}
+@import '@/scss/standardButton.scss';
 </style>
