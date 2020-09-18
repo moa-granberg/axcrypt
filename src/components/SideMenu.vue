d81d866f26b71c030b8203f26c198b262d355e9b
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-18 13:39:57 +0200

    side-menu: Styling

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -29,4 +29,32 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
+
+.side-menu-wrapper {
+  padding: 42px;
+}
+
+.side-menu-heading {
+  color: $green;
+  font-weight: 400;
+}
+
+.side-menu-links-wrapper {
+  @include no-margin-padding;
+  list-style: none;
+}
+
+.side-menu-link-item {
+  padding: 5px 10px;
+
+  a {
+    color: $gray;
+    font-weight: 400;
+
+    &.router-link-active {
+      font-weight: 600;
+      color: $green;
+    }
+  }
+}
 </style>
