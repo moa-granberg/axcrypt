d0ba7b3105aee19b128a305745dce5c63630883a
Moa Granberg <moa.granberg@hotmail.com>
2020-09-21 10:08:00 +0200

    Info-note templating with data & locales

--- src/components/information/get-started/GetStartedWindows.vue
+++ src/components/information/get-started/GetStartedWindows.vue
@@ -1,12 +1,50 @@
 <template>
   <section>
     <get-started-instruction-block :data="installingAxcryptData" />
+
+    <article :class="['information-note-wrapper', $mq]">
+      <h1 :class="['information-note-heading heading-small', $mq]">
+        {{ $t(infoNoteData1.heading) }}
+      </h1>
+      <img
+        :class="['information-note-img', $mq]"
+        src="@/assets/view/get-started/information-note-img1.svg"
+        alt=""
+      />
+      <ul :class="['information-note-list-wrapper', $mq]">
+        <li
+          :class="['information-note-list-item', $mq]"
+          v-for="item of infoNoteData1.list"
+          :key="item"
+        >
+          <p v-html="$t(item)" :class="['body-text', $mq]"></p>
+        </li>
+      </ul>
+    </article>
+
     <get-started-instruction-block :data="howToUseData" :reverse="true" />
     <get-started-instruction-block :data="keySharingData" />
     <get-started-instruction-block
       :data="passwordManagementData"
       :reverse="true"
     />
+
+    <article :class="['information-note-wrapper', $mq]">
+      <h1 :class="['information-note-heading heading-small', $mq]">
+        {{ $t(infoNoteData2.heading) }}
+      </h1>
+      <img
+        :class="['information-note-img', $mq]"
+        src="@/assets/view/get-started/information-note-img2.svg"
+        alt=""
+      />
+      <p
+        v-for="item of infoNoteData2.texts"
+        :key="item"
+        v-html="$t(item)"
+        :class="['body-text', $mq]"
+      ></p>
+    </article>
   </section>
 </template>
 
@@ -24,6 +62,18 @@ export default {
       howToUseData: {},
       keySharingData: {},
       passwordManagementData: {},
+      infoNoteData1: {
+        heading: 'GetStartedInfoNote1Heading',
+        list: [
+          'GetStartedInfoNote1ListItem1',
+          'GetStartedInfoNote1ListItem2',
+          'GetStartedInfoNote1ListItem3',
+        ],
+      },
+      infoNoteData2: {
+        heading: 'SupportLinkLabel',
+        texts: ['GetStartedInfoNote2Text1', 'GetStartedInfoNote2Text2'],
+      },
     };
   },
 
