316d8347425d6b5b50bef9590b6889d46cadf798
Moa Granberg <moa.granberg@hotmail.com>
2020-09-21 10:42:04 +0200

    Style info-notes mobile & desktop. Mixin for list-bullets

--- src/components/information/get-started/GetStartedWindows.vue
+++ src/components/information/get-started/GetStartedWindows.vue
@@ -11,15 +11,17 @@
         src="@/assets/view/get-started/information-note-img1.svg"
         alt=""
       />
-      <ul :class="['information-note-list-wrapper', $mq]">
-        <li
-          :class="['information-note-list-item', $mq]"
-          v-for="item of infoNoteData1.list"
-          :key="item"
-        >
-          <p v-html="$t(item)" :class="['body-text', $mq]"></p>
-        </li>
-      </ul>
+      <div :class="['information-note-inner-wrapper', $mq]">
+        <ul :class="['information-note-list-wrapper', $mq]">
+          <li
+            :class="['information-note-list-item', $mq]"
+            v-for="item of infoNoteData1.list"
+            :key="item"
+          >
+            <p v-html="$t(item)" :class="['body-text', $mq]"></p>
+          </li>
+        </ul>
+      </div>
     </article>
 
     <get-started-instruction-block :data="howToUseData" :reverse="true" />
@@ -38,12 +40,14 @@
         src="@/assets/view/get-started/information-note-img2.svg"
         alt=""
       />
-      <p
-        v-for="item of infoNoteData2.texts"
-        :key="item"
-        v-html="$t(item)"
-        :class="['body-text', $mq]"
-      ></p>
+      <div :class="['information-note-inner-wrapper', $mq]">
+        <p
+          v-for="item of infoNoteData2.texts"
+          :key="item"
+          v-html="$t(item)"
+          :class="['body-text', $mq]"
+        ></p>
+      </div>
     </article>
   </section>
 </template>
@@ -98,4 +102,64 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
+
+.information-note-wrapper {
+  display: grid;
+  background-color: $dark-green;
+  box-shadow: $standard-box-shadow;
+
+  &.mobile {
+    grid: auto 1fr / 2fr 1fr;
+    grid-template-areas: 'heading img' 'content content';
+
+    padding: 24px;
+  }
+
+  &.desktop {
+    grid: auto 1fr / 1fr 1fr;
+    grid-template-areas: 'img heading' 'img content';
+
+    padding: 34px;
+  }
+}
+
+.information-note-inner-wrapper {
+  grid-area: content;
+
+  background-color: $white;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 16px 0 0 0;
+}
+
+.information-note-heading {
+  grid-area: heading;
+  @include no-margin-padding;
+  color: $white;
+  font-weight: 400;
+}
+
+.information-note-img {
+  grid-area: img;
+  justify-self: center;
+
+  &.mobile {
+    width: 100px;
+    margin: -56px 0 0 0;
+  }
+
+  &.desktop {
+    align-self: center;
+    width: 180px;
+  }
+}
+
+.information-note-list-wrapper {
+  @include no-margin-padding;
+  list-style: none;
+}
+
+.information-note-list-item {
+  @include list-bullet;
+}
 </style>
