6941b1c75e2b3f500280a479a512257b51e6386c
Moa Granberg <moa.granberg@hotmail.com>
2020-09-21 16:31:34 +0200

    Benefits-wrapper template & style for mobile & desktop

--- src/pages/information/ResellerPage.vue
+++ src/pages/information/ResellerPage.vue
@@ -11,20 +11,87 @@
       </div>
       <img src="@/assets/view/reseller/resell.png" alt="resell" />
     </article>
+
+    <article :class="['reseller-benefits-wrapper', $mq]">
+      <h1 :class="['reseller-benefits-heading heading-small', $mq]">
+        {{ $t('ResellerBenefitsHeading') }}
+      </h1>
+      <p :class="['reseller-benefits-text body-text', $mq]">
+        {{ $t('ResellerBenefitsText') }}
+      </p>
+
+      <h2 :class="['reseller-benefits-offer-heading body-text-large', $mq]">
+        {{ $t('WeOfferLabel') }}
+      </h2>
+      <ul :class="['reseller-benefits-offers-wrapper', $mq]">
+        <li
+          :class="['reseller-benefits-offers-items', $mq]"
+          v-for="item of benefits"
+          :key="item.phraseKey"
+        >
+          <img
+            :class="['reseller-benefits-offers-img', $mq]"
+            :src="require(`@/assets/icons/${item.src}`)"
+            alt=""
+          />
+          <p :class="['reseller-benefits-offer-text body-text', $mq]">
+            {{ $t(item.phraseKey) }}
+          </p>
+        </li>
+      </ul>
+
+      <primary-button
+        :class="['reseller-benefits-button', $mq]"
+        phraseKey="ApplyNowLabel"
+        size="small"
+        path="/resellers-form"
+      />
+    </article>
   </main>
 </template>
 
 <script>
-export default {};
+import PrimaryButton from '@/components/PrimaryButton';
+
+export default {
+  components: {
+    PrimaryButton,
+  },
+
+  data() {
+    return {
+      benefits: [
+        {
+          phraseKey: 'BusinessFeatureListBestEncryption',
+          src: 'check.svg',
+        },
+        {
+          phraseKey: 'BusinessFeatureListMadeInSweden',
+          src: 'outlined_flag.svg',
+        },
+        {
+          phraseKey: 'FeaturesProgressiveMargins',
+          src: 'monetization_on.svg',
+        },
+        {
+          phraseKey: 'FeaturesRecurringRevenue',
+          src: 'repeat.svg',
+        },
+        {
+          phraseKey: 'FeaturesChangeToManage',
+          src: 'markunread_mailbox.svg',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
 
 .reseller-wrapper {
-  &.mobile {
-    margin: auto; //remove??
-  }
+  width: 100%;
 }
 
 .reseller-info-wrapper {
@@ -52,5 +119,95 @@ export default {};
 .reseller-info-heading {
   font-weight: 400;
   color: $green;
+}
+
+.reseller-benefits-wrapper {
+  background-color: rgba($black, 0.8);
+  @include center-column;
+
+  &.mobile {
+    padding: 24px;
+  }
+
+  &.desktop {
+    padding: 34px;
+  }
+}
+
+.reseller-benefits-heading {
+  width: 100%;
+  color: $white;
+  text-align: center;
+}
+
+.reseller-benefits-text {
+  max-width: 600px;
+  color: $white;
+  text-align: center;
+
+  &.mobile {
+    margin: 0 0 24px 0;
+  }
+}
+
+.reseller-benefits-offer-heading {
+  color: $white;
+}
+
+.reseller-benefits-offers-wrapper {
+  @include no-margin-padding;
+  list-style: none;
+  background-color: $white;
+  border-radius: 5px;
+
+  li:last-of-type {
+    border: none;
+  }
+
+  &.desktop {
+    max-width: 500px;
+  }
+}
+
+.reseller-benefits-offers-items {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid $light-gray;
+
+  &.mobile {
+    padding: 18px;
+  }
+
+  &.desktop {
+    padding: 20px;
+  }
+}
+
+.reseller-benefits-offer-text {
+  @include no-margin-padding;
+}
+
+.reseller-benefits-offers-img {
+  width: 30px;
+
+  &.mobile {
+    margin: 0 10px 0 0;
+  }
+
+  &.desktop {
+    margin: 0 20px 0 0;
+  }
+}
+
+.reseller-benefits-button {
+  text-transform: uppercase;
+
+  &.mobile {
+    margin: 18px;
+  }
+
+  &.desktop {
+    margin: 32px;
+  }
 }
 </style>
