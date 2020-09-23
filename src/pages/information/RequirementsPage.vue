e59514324b05c7a099dde7c3971ee540a1b4e13a
Moa Granberg <moa.granberg@hotmail.com>
2020-09-23 14:28:50 +0200

    Requirement text-blocks style & template. Mixin for yellow note

--- src/pages/information/RequirementsPage.vue
+++ src/pages/information/RequirementsPage.vue
@@ -1,3 +1,57 @@
 <template>
-  <main>This is Requirements</main>
+  <section :class="['requirements-wrapper', $mq]">
+    <p :class="['requirements-table-text body-text', $mq]">
+      {{ $t('RequirementsTableDescription') }}
+    </p>
+
+    <!-- table -->
+
+    <article :class="['requirements-note-wrapper', $mq]">
+      <p :class="['body-text', $mq]">
+        <strong>{{ $t('NoteLabel') }}: </strong>{{ $t('RequirementsNoteText') }}
+      </p>
+    </article>
+
+    <h1 :class="['requirements-linux-heading heading-small', $mq]">Linux</h1>
+    <p :class="['requirements-linux-text body-text', $mq]">
+      {{ $t('RequirementsLinuxText') }}
+    </p>
+  </section>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.requirements-wrapper {
+  max-width: 900px;
+
+  &.mobile {
+    padding: 24px;
+  }
+
+  &.desktop {
+    margin: 60px auto;
+  }
+}
+
+.requirements-table-text {
+  @include no-margin-padding;
+}
+
+.requirements-note-wrapper {
+  @include note-wrapper-yellow;
+}
+
+.requirements-linux-heading {
+  color: $green;
+  font-weight: 400;
+}
+
+.requirements-linux-text {
+  @include no-margin-padding;
+}
+</style>
