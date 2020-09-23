1d2838f38631e1c471635bb5841150b80d2eb28b
Moa Granberg <moa.granberg@hotmail.com>
2020-09-23 18:18:26 +0200

    Style mobile & desktop

--- src/pages/information/UsageTipsPage.vue
+++ src/pages/information/UsageTipsPage.vue
@@ -5,27 +5,31 @@
     </p>
 
     <article :class="['usage-tips-article', $mq]">
-      <h1>{{ $t('UsageTipsTargetUseHeading') }}</h1>
+      <h1 :class="['heading-medium', $mq]">
+        {{ $t('UsageTipsTargetUseHeading') }}
+      </h1>
       <ul :class="['usage-tips-list', $mq]">
         <li
           v-for="item of targetUseList"
           :key="item"
           :class="['usage-tips-list-item', $mq]"
         >
-          <p>{{ $t(item) }}</p>
+          <p :class="['body-text', $mq]">{{ $t(item) }}</p>
         </li>
       </ul>
     </article>
 
     <article :class="['usage-tips-article', $mq]">
-      <h1>{{ $t('UsageTipsPersonalPrivacyHeading') }}</h1>
+      <h1 :class="['heading-medium', $mq]">
+        {{ $t('UsageTipsPersonalPrivacyHeading') }}
+      </h1>
       <ul :class="['usage-tips-list', $mq]">
         <li
           v-for="item of personalPrivacyList"
           :key="item"
           :class="['usage-tips-list-item', $mq]"
         >
-          <p>{{ $t(item) }}</p>
+          <p :class="['body-text', $mq]">{{ $t(item) }}</p>
         </li>
       </ul>
     </article>
@@ -35,8 +39,10 @@
       :key="obj.heading"
       :class="['usage-tips-article', $mq]"
     >
-      <h1>{{ $t(obj.heading) }}</h1>
-      <p v-for="text of obj.texts" :key="text">{{ $t(text) }}</p>
+      <h1 :class="['heading-medium', $mq]">{{ $t(obj.heading) }}</h1>
+      <p v-for="text of obj.texts" :key="text" :class="['body-text', $mq]">
+        {{ $t(text) }}
+      </p>
     </article>
   </section>
 </template>
@@ -59,17 +65,48 @@ export default {
 @import '@/scss/variables.scss';
 
 .usage-tips-wrapper {
+  max-width: 900px;
+
+  &.mobile {
+    padding: 24px;
+  }
 }
 
 .usage-tips-description {
+  text-align: center;
+  max-width: 600px;
+
+  &.mobile {
+    margin: 0 auto 14px auto;
+  }
+
+  &.desktop {
+    margin: 58px auto 0 auto;
+  }
 }
 
 .usage-tips-article {
+  &.mobile {
+    padding: 14px 0;
+  }
+
+  &.desktop {
+    padding: 20px 0;
+  }
+
+  h1 {
+    @include no-margin-padding;
+    color: $green;
+    font-weight: 400;
+  }
 }
 
 .usage-tips-list {
+  @include no-margin-padding;
+  list-style: none;
 }
 
 .usage-tips-list-item {
+  @include list-bullet;
 }
 </style>
