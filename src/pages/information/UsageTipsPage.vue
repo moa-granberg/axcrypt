74d46ae32939ae5a829fecb3b0bfdfaaf7b89586
Moa Granberg <moa.granberg@hotmail.com>
2020-09-23 17:42:52 +0200

    Set up template & data

--- src/pages/information/UsageTipsPage.vue
+++ src/pages/information/UsageTipsPage.vue
@@ -1,3 +1,75 @@
 <template>
-  <main>This is Usage Tips</main>
+  <section :class="['usage-tips-wrapper', $mq]">
+    <p :class="['usage-tips-description', $mq]">
+      {{ $t('UsageTipsDescription') }}
+    </p>
+
+    <article :class="['usage-tips-article', $mq]">
+      <h1>{{ $t('UsageTipsTargetUseHeading') }}</h1>
+      <ul :class="['usage-tips-list', $mq]">
+        <li
+          v-for="item of targetUseList"
+          :key="item"
+          :class="['usage-tips-list-item', $mq]"
+        >
+          <p>{{ $t(item) }}</p>
+        </li>
+      </ul>
+    </article>
+
+    <article :class="['usage-tips-article', $mq]">
+      <h1>{{ $t('UsageTipsPersonalPrivacyHeading') }}</h1>
+      <ul :class="['usage-tips-list', $mq]">
+        <li
+          v-for="item of personalPrivacyList"
+          :key="item"
+          :class="['usage-tips-list-item', $mq]"
+        >
+          <p>{{ $t(item) }}</p>
+        </li>
+      </ul>
+    </article>
+
+    <article
+      v-for="obj of infoBlockList"
+      :key="obj.heading"
+      :class="['usage-tips-article', $mq]"
+    >
+      <h1>{{ $t(obj.heading) }}</h1>
+      <p v-for="text of obj.texts" :key="text">{{ $t(text) }}</p>
+    </article>
+  </section>
 </template>
+
+<script>
+import data from '@/data/information/usage-tips/data';
+
+export default {
+  data() {
+    return {
+      targetUseList: data.targetUseList,
+      personalPrivacyList: data.personalPrivacyList,
+      infoBlockList: data.infoBlockList,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.usage-tips-wrapper {
+}
+
+.usage-tips-description {
+}
+
+.usage-tips-article {
+}
+
+.usage-tips-list {
+}
+
+.usage-tips-list-item {
+}
+</style>
