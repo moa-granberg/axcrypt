0b1eaaafeb6f476259a50c06f6ea54c5e9cc40f1
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-24 09:43:33 +0200

    features-page: more-features styling mobile & desktop, data fixed to reflect different plans better

--- src/components/information/features/FeaturesMoreFeatures.vue
+++ src/components/information/features/FeaturesMoreFeatures.vue
@@ -17,14 +17,14 @@
 
       <div
         :class="['more-features-item-wrapper', $mq]"
-        v-for="item of moreFeaturesList"
+        v-for="item of currentList"
         :key="item.featurePhraseKey"
       >
-        <p :class="['more-features-item-label', $mq]">
+        <p :class="['more-features-item-label body-text', $mq]">
           {{ $t(item.featurePhraseKey) }}
         </p>
 
-        <p :class="['more-features-item-description', $mq]">
+        <p :class="['more-features-item-description body-text', $mq]">
           {{ $t(item.descriptionPhraseKey) }}
         </p>
       </div>
@@ -36,10 +36,28 @@
 import MoreFeaturesListJson from '@/data/information/features/features-more-features';
 
 export default {
+  props: {
+    plan: String,
+  },
+
   data() {
     return {
       moreFeaturesList: MoreFeaturesListJson,
     };
+  },
+
+  computed: {
+    currentList() {
+      if (this.plan === 'free') {
+        return this.moreFeaturesList.filter(item => item.free === true);
+      } else if (this.plan === 'premium') {
+        return this.moreFeaturesList.filter(item => item.premium === true);
+      } else if (this.plan === 'business') {
+        return this.moreFeaturesList.filter(item => item.business === true);
+      } else {
+        return null;
+      }
+    },
   },
 };
 </script>
@@ -47,27 +65,93 @@ export default {
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
 
+$list-grid: 1fr / 1fr 2fr;
+
 .more-features-wrapper {
+  margin: 36px 24px;
 }
 
 .more-features-heading {
+  &.mobile {
+    @include no-margin-padding;
+    padding: 16px;
+    border-radius: 5px 5px 0 0;
+    border: 1px solid $light-gray;
+    border-bottom: none;
+    background-color: $green;
+    color: $white;
+    text-align: center;
+    font-weight: 400;
+  }
+
+  &.desktop {
+    color: $green;
+  }
 }
 
 .more-features-table-wrapper {
+  &.mobile {
+    border: 1px solid $light-gray;
+    border-radius: 0 0 5px 5px;
+    padding: 12px;
+    box-shadow: $standard-box-shadow;
+  }
+
+  &.desktop {
+    border: 1px solid $light-gray;
+    border-radius: 5px;
+  }
 }
 
 .more-features-table-headings-wrapper {
+  &.mobile {
+    display: none;
+  }
+  &.desktop {
+    display: grid;
+    grid: $list-grid;
+    border-radius: 5px 5px 0 0;
+    padding: 0 24px;
+    background-color: $green;
+  }
 }
 
 .more-features-table-heading {
+  &.desktop {
+    color: $white;
+    font-weight: 600;
+  }
 }
 
 .more-features-item-wrapper {
+  border-bottom: 1px solid $gray;
+
+  &:last-of-type {
+    border: none;
+  }
+
+  &.mobile {
+    padding: 12px;
+  }
+
+  &.desktop {
+    padding: 12px;
+    margin: 0 12px;
+    display: grid;
+    grid: $list-grid;
+  }
 }
 
 .more-features-item-label {
+  @include no-margin-padding;
+  font-weight: 600;
+
+  &.mobile {
+    margin: 0 0 10px 0;
+  }
 }
 
 .more-features-item-description {
+  @include no-margin-padding;
 }
 </style>
