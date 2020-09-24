009e0adb4e89dbb31fb70fa1da1e616b447727a3
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2020-09-24 10:38:36 +0200

    Mobile header fade-layer (#34)
    * Fade-layer added with style
    
    * FadeLayer .show style
    
    * FadeLayer functionality

--- src/components/header/HeaderComp.vue
+++ src/components/header/HeaderComp.vue
@@ -1,5 +1,11 @@
 <template>
   <header :class="'header ' + this.$mq">
+    <section
+      v-if="$mq === 'mobile'"
+      :class="[{ show: showMobileMenu }, 'fade-layer', $mq]"
+      @click="showMobileMenu = false"
+    ></section>
+
     <section :class="'header-wrapper ' + this.$mq">
       <div :class="'nav ' + this.$mq">
         <router-link to="/" @click.native="showMobileMenu = false">
@@ -50,12 +56,23 @@ export default {
 }
 
 .header {
+  &.mobile {
+    position: relative;
+  }
+
   &.desktop {
     border-bottom: 1px solid $light-gray;
   }
 }
 
 .header-wrapper {
+  background-color: $white;
+
+  &.mobile {
+    z-index: 2;
+    position: relative;
+  }
+
   &.desktop {
     display: flex;
     justify-content: space-between;
@@ -76,6 +93,18 @@ export default {
   &.desktop {
     border: none;
     margin: 0;
+  }
+}
+
+.fade-layer {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+
+  &.show {
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba($black, 0.6);
   }
 }
 </style>
