530bae58b965a96e170bd1fd09ba37c9b79637bd
Moa Granberg <moa.granberg@hotmail.com>
2020-09-25 14:29:54 +0200

    Templating latest releaseNotes from data, added locales

--- src/pages/information/ReleaseNotesPage.vue
+++ src/pages/information/ReleaseNotesPage.vue
@@ -1,3 +1,27 @@
 <template>
-  <main>This is Release Notes</main>
+  <section :class="['text-view-wrapper', $mq]">
+    <p :class="['description', $mq]" v-html="$t('ReleaseNotesDescription')" />
+
+    <article :class="$mq" v-for="note of releaseNotes" :key="note.heading">
+      <h1 :class="$mq">{{ note.heading }}</h1>
+      <p :class="$mq">{{ note.summary }}</p>
+      <ul :class="$mq" v-if="note.changeLog">
+        <li :class="$mq" v-for="code of note.changeLog" :key="code">
+          <p :class="$mq">{{ code }}</p>
+        </li>
+      </ul>
+    </article>
+  </section>
 </template>
+
+<script>
+import releaseNotes from '@/data/information/release-notes/data';
+
+export default {
+  data() {
+    return {
+      releaseNotes,
+    };
+  },
+};
+</script>
