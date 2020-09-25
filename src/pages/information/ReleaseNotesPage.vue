b52aeb748ef8fcdbbcebd10b7fa2a79d3a62358c
Moa Granberg <moa.granberg@hotmail.com>
2020-09-25 15:06:15 +0200

    Render pdf-links

--- src/pages/information/ReleaseNotesPage.vue
+++ src/pages/information/ReleaseNotesPage.vue
@@ -11,6 +11,21 @@
         </li>
       </ul>
     </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2018</h1>
+      <a href="pdfs/ReleaseNotes2018.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2018</a>
+    </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2017</h1>
+      <a href="pdfs/ReleaseNotes2017.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2017</a>
+    </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2016</h1>
+      <a href="pdfs/ReleaseNotes2016.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2016</a>
+    </article>
   </section>
 </template>
 
