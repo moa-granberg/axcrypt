fa32918b3e16b8c631099114ce1335ffdc515a45
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2020-09-25 15:14:34 +0200

    Release notes (#40)
    * Release notes data
    
    * Templating latest releaseNotes from data, added locales
    
    * Render pdf-links
    
    * All a-tags in text-view-wrapper same style
    
    Co-authored-by: Daniel <daniel.holbling@gmail.com>

--- src/pages/information/ReleaseNotesPage.vue
+++ src/pages/information/ReleaseNotesPage.vue
@@ -1,3 +1,42 @@
 <template>
-  <main>This is Release Notes</main>
+  <section :class="['text-view-wrapper', $mq]">
+    <p :class="['description', $mq]" v-html="$t('ReleaseNotesDescription')" />
+
+    <article :class="$mq" v-for="note of releaseNotes" :key="note.heading">
+      <h1 :class="$mq">{{ note.heading }}</h1>
+      <p :class="$mq">{{ note.summary }}</p>
+      <ul :class="$mq" v-if="note.changeLog">
+        <li :class="$mq" v-for="code of note.changeLog" :key="code">
+          <p :class="$mq">{{ code }}</p>
+        </li>
+      </ul>
+    </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2018</h1>
+      <a href="pdfs/ReleaseNotes2018.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2018</a>
+    </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2017</h1>
+      <a href="pdfs/ReleaseNotes2017.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2017</a>
+    </article>
+
+    <article :class="$mq">
+      <h1 :class="$mq">2016</h1>
+      <a href="pdfs/ReleaseNotes2016.pdf" target="_blank"> {{$t('ReleaseNotesYearLabel')}} 2016</a>
+    </article>
+  </section>
 </template>
+
+<script>
+import releaseNotes from '@/data/information/release-notes/data';
+
+export default {
+  data() {
+    return {
+      releaseNotes,
+    };
+  },
+};
+</script>
