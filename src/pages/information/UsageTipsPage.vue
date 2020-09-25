fbb60c9acaec0d44b116aedc087aad736116a48e
Moa Granberg <moa.granberg@hotmail.com>
2020-09-25 13:06:50 +0200

    Scss-file for general templating of text-view's. Imported globally in App & used in UsageTipsPage.

--- src/pages/information/UsageTipsPage.vue
+++ src/pages/information/UsageTipsPage.vue
@@ -1,46 +1,34 @@
 <template>
-  <section :class="['usage-tips-wrapper', $mq]">
-    <p :class="['usage-tips-description', $mq]">
+  <section :class="['text-view-wrapper', $mq]">
+    <p :class="['description', $mq]">
       {{ $t('UsageTipsDescription') }}
     </p>
 
-    <article :class="['usage-tips-article', $mq]">
-      <h1 :class="['heading-medium-green', $mq]">
+    <article :class="$mq">
+      <h1 :class="$mq">
         {{ $t('UsageTipsTargetUseHeading') }}
       </h1>
-      <ul :class="['usage-tips-list', $mq]">
-        <li
-          v-for="item of targetUseList"
-          :key="item"
-          :class="['usage-tips-list-item', $mq]"
-        >
-          <p :class="['body-text', $mq]">{{ $t(item) }}</p>
+      <ul :class="$mq">
+        <li v-for="item of targetUseList" :key="item" :class="$mq">
+          <p :class="$mq">{{ $t(item) }}</p>
         </li>
       </ul>
     </article>
 
-    <article :class="['usage-tips-article', $mq]">
-      <h1 :class="['heading-medium-green', $mq]">
+    <article :class="$mq">
+      <h1 :class="$mq">
         {{ $t('UsageTipsPersonalPrivacyHeading') }}
       </h1>
-      <ul :class="['usage-tips-list', $mq]">
-        <li
-          v-for="item of personalPrivacyList"
-          :key="item"
-          :class="['usage-tips-list-item', $mq]"
-        >
-          <p :class="['body-text', $mq]">{{ $t(item) }}</p>
+      <ul :class="$mq">
+        <li v-for="item of personalPrivacyList" :key="item" :class="$mq">
+          <p :class="$mq">{{ $t(item) }}</p>
         </li>
       </ul>
     </article>
 
-    <article
-      v-for="obj of infoBlockList"
-      :key="obj.heading"
-      :class="['usage-tips-article', $mq]"
-    >
-      <h1 :class="['heading-medium-green', $mq]">{{ $t(obj.heading) }}</h1>
-      <p v-for="text of obj.texts" :key="text" :class="['body-text', $mq]">
+    <article v-for="obj of infoBlockList" :key="obj.heading" :class="$mq">
+      <h1 :class="$mq">{{ $t(obj.heading) }}</h1>
+      <p v-for="text of obj.texts" :key="text" :class="$mq">
         {{ $t(text) }}
       </p>
     </article>
@@ -60,48 +48,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import '@/scss/variables.scss';
-
-.usage-tips-wrapper {
-  max-width: $max-content-width;
-  margin: auto;
-
-  &.mobile {
-    padding: $margin-mobile;
-  }
-}
-
-.usage-tips-description {
-  text-align: center;
-  max-width: $max-text-width;
-
-  &.mobile {
-    margin: 0 auto 14px auto;
-  }
-
-  &.desktop {
-    margin: $margin-top-aside-content auto 0 auto;
-  }
-}
-
-.usage-tips-article {
-  &.mobile {
-    padding: 14px 0;
-  }
-
-  &.desktop {
-    padding: 20px 0;
-  }
-}
-
-.usage-tips-list {
-  @include no-margin-padding;
-  list-style: none;
-}
-
-.usage-tips-list-item {
-  @include list-bullet;
-}
-</style>
