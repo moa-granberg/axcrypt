9546140bbcf9447da0da652cd5d7b172a28f2ef3
Moa Granberg <moa.granberg@hotmail.com>
2020-09-29 09:45:35 +0200

    History-article template & style for mobile & desktop

--- src/pages/about/AboutAxcryptPage.vue
+++ src/pages/about/AboutAxcryptPage.vue
@@ -1,9 +1,40 @@
 <template>
-  <main>This is About Axcrypt</main>
+  <section :class="['about-axcrypt-wrapper', $mq]">
+    <article :class="['history-wrapper', $mq]">
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText1') }}</p>
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText2') }}</p>
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText3') }}</p>
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText4') }}</p>
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText5') }}</p>
+      <p :class="['body-text', $mq]">{{ $t('AboutAxcryptHistoryText6') }}</p>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+
+.about-axcrypt-wrapper {
+  @include standard-margin;
+
+  &.desktop {
+    margin-top: $margin-top-aside-content;
+  }
+}
+
+.history-wrapper {
+  &.desktop {
+    max-width: $max-content-width;
+    margin: auto;
+    column-count: 2;
+  }
+
+  p:first-of-type {
+    margin-top: 0;
+  }
+}
+</style>
