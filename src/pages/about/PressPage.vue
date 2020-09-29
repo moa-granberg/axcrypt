a115beadbd30ad2b2c8a591cf76cd1a15888e09b
Moa Granberg <moa.granberg@hotmail.com>
2020-09-29 14:53:31 +0200

    Press images-article style, template for mobile & desktop

--- src/pages/about/PressPage.vue
+++ src/pages/about/PressPage.vue
@@ -17,6 +17,35 @@
         </p>
       </div>
     </article>
+
+    <article :class="['press-images-wrapper', $mq]">
+      <h1 :class="[$mq]">{{ $t('PressImagesLabel') }}</h1>
+      <p :class="[$mq]">{{ $t('PressImagesDescription') }}</p>
+
+      <div :class="['press-images-image-wrapper', $mq]">
+        <div :class="[$mq]">
+          <img
+            :class="['press-img', $mq]"
+            src="@/assets/team-photos/zebastian-500x500.jpg"
+            alt="Zebastian"
+          />
+          <h2 :class="['name-heading heading-small', $mq]">
+            Zebastian Victorin
+          </h2>
+          <p :class="[$mq]">Co-founder / CEO</p>
+        </div>
+
+        <div :class="[$mq]">
+          <img
+            :class="['press-img', $mq]"
+            src="@/assets/team-photos/mans-500x500.jpg"
+            alt="Måns"
+          />
+          <h2 :class="['name-heading heading-small', $mq]">Måns Hansson</h2>
+          <p :class="[$mq]">Co-founder / Creative director</p>
+        </div>
+      </div>
+    </article>
   </section>
 </template>
 
@@ -33,6 +62,11 @@ export default {};
 
   > article {
     @include standard-padding;
+    margin: auto;
+  }
+
+  &.desktop {
+    max-width: $sidemenu-served-content-width !important;
   }
 }
 
@@ -43,6 +77,37 @@ export default {};
 
   &.desktop {
     margin: $margin-desktop 0 0 0;
+  }
+}
+
+.press-images-wrapper {
+  background-color: $light-gray;
+}
+
+.press-images-image-wrapper {
+  &.desktop {
+    margin: $margin-desktop 0 0 0;
+    @include center-row;
+    gap: $margin-desktop;
+  }
+}
+
+.press-images-image-wrapper p {
+  @include no-margin-padding;
+}
+
+.press-images-image-wrapper .name-heading {
+  font-weight: 600;
+}
+
+.press-img {
+  &.mobile {
+    max-width: 80vw;
+    margin: $margin-mobile 0 0 0;
+  }
+
+  &.desktop {
+    max-width: 100%;
   }
 }
 </style>
