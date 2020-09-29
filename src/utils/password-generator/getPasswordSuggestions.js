471d949b31d8759ee3acab3065d201e3a123a683
Daniel <daniel.holbling@gmail.com>
2020-09-29 09:34:16 +0200

    Password generator page (#50)
    * password-generator-page: templating & locales
    
    * password-generator-page: utility for fetching password suggestions
    
    * password-generator-page: Added func in template and test env func
    
    * password-generator-page: styling
    
    * package-lock
    
    * password-generator-page: Added more air in passwords display

---
+++ src/utils/password-generator/getPasswordSuggestions.js
@@ -0,0 +1,51 @@
+// PRODUCTION ENV
+
+// const getPasswordSuggestion = async strength => {
+//   let resp;
+
+//   if (strength === 'weak') {
+//     resp = await fetch(
+//       'https://account.axcrypt.net/api/global/passwords/suggestion/1',
+//       { mode: 'no-cors' }
+//     );
+//     resp = await resp.json();
+//     return resp.suggestion;
+//   } else if (strength === 'medium') {
+//     resp = await fetch(
+//       'https://account.axcrypt.net/api/global/passwords/suggestion/2',
+//       { mode: 'no-cors' }
+//     );
+
+//     resp = await resp.json();
+//     return resp.suggestion;
+//   } else if (strength === 'strong') {
+//     resp = await fetch(
+//       'https://account.axcrypt.net/api/global/passwords/suggestion/3',
+//       { mode: 'no-cors' }
+//     );
+
+//     resp = await resp.json();
+//     return resp.suggestion;
+//   } else {
+//     return 'Invalid argument, valid arguments are "strong", "medium" or "weak"';
+//   }
+// };
+
+// export const getPasswords = async () => {
+//   let resp = {};
+
+//   resp.weak = await getPasswordSuggestion('weak');
+//   resp.medium = await getPasswordSuggestion('medium');
+//   resp.strong = await getPasswordSuggestion('strong');
+
+//   return resp;
+// };
+
+// TEST ENV
+export const getPasswords = async () => {
+  return {
+    weak: 'tEntsent',
+    medium: 'MegnitTeen33',
+    strong: '-Fediabs86sewdEree',
+  };
+};
