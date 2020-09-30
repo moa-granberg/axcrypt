4608ec082973003c35b0678172406fb1ce2ae7f2
Moa Granberg <moa.granberg@hotmail.com>
2020-09-30 13:15:49 +0200

    Job-page style mobile & desktop

--- src/pages/about/JobsPage.vue
+++ src/pages/about/JobsPage.vue
@@ -6,10 +6,9 @@
       :class="['job-article', $mq]"
     >
       <h1 :class="['job-heading heading-medium', $mq]">{{ job.position }}</h1>
-      <h2 :class="['job-subheading heading-small', $mq]">{{ job.location }}</h2>
+      <h2 :class="['job-heading body-text-large', $mq]">{{ job.location }}</h2>
       <p :class="['job-description body-text', $mq]">{{ job.summary }}</p>
       <primary-button
-        :class="['job-button', $mq]"
         phraseKey="ReadMoreButtonLabel"
         size="small"
         :path="`/about/jobs/${job.id}`"
@@ -37,4 +36,39 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/scss/variables.scss';
+
+.jobs-wrapper {
+  @include standard-margin;
+  @include center-column;
+
+  &.mobile {
+    gap: $margin-mobile * 2;
+  }
+
+  &.desktop {
+    gap: $margin-desktop * 2;
+  }
+}
+
+.job-article {
+  @include center-column;
+}
+
+.job-heading {
+  @include no-margin-padding;
+  font-weight: 400;
+  color: $green;
+}
+
+.job-description {
+  text-align: center;
+  max-width: $max-text-width;
+
+  &.mobile {
+    margin: $margin-mobile 0;
+  }
+  &.desktop {
+    margin: $margin-desktop 0;
+  }
+}
 </style>
