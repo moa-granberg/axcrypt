67831b8854d26d03090c7782cb76fc97214ad20a
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-09-30 13:29:15 +0200

    jobs-page: templating and data mounted

--- src/pages/about/JobPositionPage.vue
+++ src/pages/about/JobPositionPage.vue
@@ -1,3 +1,44 @@
 <template>
-  <main>This is Job Position</main>
+  <section>
+    <article>
+      <h1></h1>
+      <p></p>
+      <primary-button path="/#" />
+    </article>
+
+    <article>
+      <h1></h1>
+      <ul>
+        <li><p></p></li>
+      </ul>
+    </article>
+
+    <article>
+      <h1></h1>
+      <ul>
+        <li><p></p></li>
+      </ul>
+    </article>
+  </section>
 </template>
+
+<script>
+import PrimaryButton from '@/components/PrimaryButton';
+import jobPositions from '@/data/about/jobs/job-positions';
+
+export default {
+  components: {
+    PrimaryButton,
+  },
+
+  computed: {
+    position() {
+      return jobPositions.find(
+        job => job.id === this.$route.path.split('/about/jobs/')[1]
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
