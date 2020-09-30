52501a3d45280b949b1bf5ec00542711075b66b4
Moa Granberg <moa.granberg@hotmail.com>
2020-09-30 15:16:37 +0200

    Form template with text & select-inputs, reCaptcha & validation-methods

--- src/pages/about/JobApplicationPage.vue
+++ src/pages/about/JobApplicationPage.vue
@@ -14,14 +14,99 @@
       </p>
     </article>
 
-    <form></form>
+    <form
+      v-if="data.length"
+      :class="['form-wrapper', $mq]"
+      v-on:submit.prevent="handleSubmit"
+    >
+      <article v-for="item of data" :key="item.id">
+        <div
+          :class="['input-wrapper', $mq]"
+          v-if="item.type === 'text' || item.type === 'number'"
+        >
+          <label :for="item.id" :class="['body-text', $mq]">
+            {{ item.text }}
+          </label>
+          <input
+            type="text"
+            :id="item.id"
+            v-model="response[item.id]"
+            :class="[{ invalid: item.error }, $mq]"
+            @blur="validate(item.id)"
+          />
+          <p v-if="item.error" :class="['error-msg body-text', $mq]">
+            {{ $t(item.error) }}
+          </p>
+          <p v-else :class="['error-msg body-text', $mq]"></p>
+        </div>
+
+        <div :class="['input-wrapper', $mq]" v-if="item.type === 'select'">
+          <label :for="item.id" :class="['body-text', $mq]">
+            {{ item.text }}
+          </label>
+          <select
+            :id="item.id"
+            v-model="response[item.id]"
+            :class="[{ invalid: item.error }, $mq]"
+            @blur="validate(item.id)"
+          >
+            <option v-for="value of item.options" :key="value" :value="value">
+              {{ value }}
+            </option>
+          </select>
+          <p v-if="item.error" :class="['error-msg body-text', $mq]">
+            {{ $t(item.error) }}
+          </p>
+          <p v-else :class="['error-msg body-text', $mq]"></p>
+        </div>
+      </article>
+
+      <vue-recaptcha
+        :class="['recaptcha-wrapper', $mq]"
+        @verify="handleVerify"
+        sitekey="6Lchj88ZAAAAAAYuwcZHGwZ-izWcOq4_B7LHaOyI"
+      >
+        <p v-if="robotError" :class="['error-msg robot body-text', $mq]">
+          {{ $t('ErrorConfirmNotRobot') }}
+        </p>
+      </vue-recaptcha>
+
+      <input
+        type="submit"
+        :value="$t('SubmitLabel')"
+        :class="['standard-button small', $mq]"
+      />
+    </form>
   </section>
 </template>
 
 <script>
+import VueRecaptcha from 'vue-recaptcha';
 import jobPositions from '@/data/about/jobs/job-positions';
+import data from '@/data/about/job-application/form';
+import { validate } from '@/utils/formValidation';
 
 export default {
+  components: {
+    VueRecaptcha,
+  },
+
+  data() {
+    return {
+      data,
+      response: {
+        firstName: '',
+        lastName: '',
+        phone: '',
+        email: '',
+        city: '',
+        country: '',
+      },
+      robot: false,
+      robotError: false,
+    };
+  },
+
   computed: {
     position() {
       return jobPositions.find(
@@ -29,11 +114,38 @@ export default {
       );
     },
   },
+
+  methods: {
+    handleSubmit() {
+      this.data.forEach(input => {
+        this.validate(input.id);
+        if (input.error) {
+          return;
+        }
+      });
+      if (this.robot) {
+        //send data
+      } else {
+        this.robotError = true;
+      }
+    },
+
+    handleVerify(resp) {
+      if (resp) {
+        this.robot = true;
+        this.robotError = false;
+      }
+    },
+
+    validate(id) {
+      this.data = validate(id, this.response, this.data);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '@/scss/variables';
+@import '@/scss/form';
 
 .job-application-wrapper {
   &.mobile {
