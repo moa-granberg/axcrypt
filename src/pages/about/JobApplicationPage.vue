a5431a4a3e638e4947757bbbb0aeeb340de2b39f
Moa Granberg <moa.granberg@hotmail.com>
2020-09-30 14:50:36 +0200

    JobApplication text-wrapper template, data & style for mobile & desktop

--- src/pages/about/JobApplicationPage.vue
+++ src/pages/about/JobApplicationPage.vue
@@ -1,3 +1,59 @@
 <template>
-  <main>This is Application Form</main>
+  <section :class="['job-application-wrapper', $mq]">
+    <article :class="['text-wrapper', $mq]">
+      <h1 :class="['position-heading heading-jumbo', $mq]">
+        {{ position.position }}
+      </h1>
+      <h2 :class="['position-heading heading-medium', $mq]">
+        {{ position.location }}
+      </h2>
+      <p :class="['body-text', $mq]">
+        Thank you for your interest in applying for a job at AxCrypt. Please
+        take a minute to fill out the following form. After you have completed
+        your application, we will contact you via email.
+      </p>
+    </article>
+
+    <form></form>
+  </section>
 </template>
+
+<script>
+import jobPositions from '@/data/about/jobs/job-positions';
+
+export default {
+  computed: {
+    position() {
+      return jobPositions.find(
+        job => job.id === this.$route.path.split('/about/jobs/application/')[1]
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables';
+
+.job-application-wrapper {
+  &.mobile {
+    margin: $margin-mobile;
+  }
+
+  &.desktop {
+    margin: $margin-top-aside-content 0 $margin-desktop 0;
+  }
+}
+
+.text-wrapper {
+  margin: auto;
+  max-width: $max-text-width;
+}
+
+.position-heading {
+  @include no-margin-padding;
+  text-align: center;
+  color: $green;
+  font-weight: 400;
+}
+</style>
