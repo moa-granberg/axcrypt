ee0990270df897af6a31d06849a70bd7d707f67d
Moa Granberg <moa.granberg@hotmail.com>
2020-10-06 11:29:02 +0200

    GetPricing func

--- src/utils/pricing/getPricing.js
+++ src/utils/pricing/getPricing.js
@@ -1,17 +1,10 @@
-//ONLY FOR TESTING ENV
 export const getPricing = async (product, period) => {
-  const resp = await import(`@/utils/pricing/${product}-${period}ly.json`);
-  return formatPrice(resp.default, period);
+  const resp = await fetch(
+    `https://account.axcrypt.net/api/global/pricelist/${product}/${period}/1`,
+    { mode: 'no-cors' }
+  );
+  formatPrice(resp, period);
 };
-
-//TO USE IN PRODUCTION ENV
-// export const getPricing = async (product, period) => {
-//   const resp = await fetch(
-//     `https://account.axcrypt.net/api/global/pricelist/${product}/${period}/1`,
-//     { mode: 'no-cors' }
-//   );
-//   formatPrice(resp, period);
-// };
 
 const formatPrice = (response, period) => {
   const currency = response.default_currency;
